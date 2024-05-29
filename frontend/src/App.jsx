38f8b20858a9b4f3aafe5462283a6e8286fcb3b6
Shreyas-Pattabi <spattabik@gmail.com>
2024-05-29 16:02:52 -0500

    Router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Home from './components/Home'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   )
 }
 
