2594b5132923e6631884b65fd5e52e73f4d2ba67
Akshar Chunduri <60073360+AksharChunduri@users.noreply.github.com>
2024-05-30 00:46:48 -0500

    Update Draft.jsx

--- frontend/src/components/Draft.jsx
+++ frontend/src/components/Draft.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logoBO.png'
 
 const Draft = () => {
   return (
     <div>
         <header className="header">
-        <div className="logo">Fantasy Sports</div>
+        <div className="logo">
+        <img src={logo} alt="BO Logo" />
+          <span>Ball Out</span>
+        </div>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/matchup">Team</Link></li>
+          <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Login</Link></li>
+            <li><Link to="/lineup">Lineup</Link></li>
+            <li><Link to="/matchup">Matchup</Link></li>
+            <li><Link to="/my-league">League</Link></li>
             <li><Link to="/draft">Draft</Link></li>
-            <li><Link to="/players">Players</Link></li>
+            <li><Link to="/players">Free Agents</Link></li>
           </ul>
         </nav>
       </header>
