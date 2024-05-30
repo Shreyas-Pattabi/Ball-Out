78bcacc03491da6c59a6c7dbc2df83c7fac50914
Jai-Mahajan <jai17nm@gmail.com>
2024-05-30 13:20:06 -0500

    Login Page Addition

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logoBO.png'
+import './Login.css';
+import logo from '../assets/logoBO.png';
 
 const Login = () => {
-    return (
-    
-      <div>
-            <header className="header">
+  return (
+    <div>
+      <header className="header">
         <div className="logo">
-        <img src={logo} alt="BO Logo" />
+          <img src={logo} alt="BO Logo" />
           <span>Ball Out</span>
         </div>
         <nav>
           <ul>
-          <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/lineup">Lineup</Link></li>
             <li><Link to="/matchup">Matchup</Link></li>
@@ -23,8 +23,35 @@ const Login = () => {
           </ul>
         </nav>
       </header>
+      
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
       </div>
-    );
-  };
+      
+      <form className="login-form">
+        <h3>Login Here</h3>
+        
+        <label htmlFor="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username" />
+        
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="Password" id="password" />
+        
+        <div className="login">
+          <i className="login"></i>
+          <div className="log">
+            <button type="submit">Log In</button>
+          </div>
+        </div>
+        
+        <div className="social">
+          <div className="social-button go"><i className="fab fa-google"></i> Google</div>
+          <div className="social-button fb"><i className="fab fa-facebook"></i> Facebook</div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
